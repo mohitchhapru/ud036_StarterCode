1d3a713712ae16c06f3d076982b6add4395c5360
mohitchhapru <35535834+mohitchhapru@users.noreply.github.com>
2018-03-16 21:08:52 -0700

    Update media.py
    Following Python coding standards

--- media.py
+++ media.py
@@ -1,4 +1,4 @@
-class Movie:  #Class Movie is a user defined data structure to combine details of a movie
+class Movie:  # Class Movie is a user defined data structure to combine details of a movie
     def __init__(self,title,poster_image_url,trailer_youtube_url):
         self.title = title
         self.poster_image_url = poster_image_url
