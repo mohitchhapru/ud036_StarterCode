7aecb65cb3280555c19e7dd882335393dcea5a35
mohitchhapru <35535834+mohitchhapru@users.noreply.github.com>
2018-03-17 10:47:19 -0700

    Update media.py
    Updated code according to http://pep8online.com/

--- media.py
+++ media.py
@@ -1,5 +1,6 @@
 class Movie:  # Class Movie is a user defined data structure to combine details of a movie
-    def __init__(self,title,poster_image_url,trailer_youtube_url):
+    # Constructor with paramaeters
+    def __init__(self, title, poster_image_url, trailer_youtube_url):
         self.title = title  #: This field contains the name of the movie
         self.poster_image_url = poster_image_url  #: This field contains the URL of the movie poster
         self.trailer_youtube_url = trailer_youtube_url  #: This field contains the YouTube URL of the movie trailer
